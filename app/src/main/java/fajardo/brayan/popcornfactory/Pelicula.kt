949a326b08dbab96af5313ff7a479183c40f146e
Brayan-Alexis <brayanzxd@gmail.com>
2021-03-08 01:30:28 -0700

    Finish

---
+++ app/src/main/java/fajardo/brayan/popcornfactory/Pelicula.kt
@@ -0,0 +1,10 @@
+package fajardo.brayan.popcornfactory
+
+data class Pelicula(var titulo: String,
+                    var image: Int,
+                    var header: Int,
+                    var sinopsis: String) {
+
+
+
+}
---
+++ app/src/main/java/fajardo/brayan/popcornfactory/Serie.kt
@@ -0,0 +1,7 @@
+package fajardo.brayan.popcornfactory
+
+data class Serie(var titulo: String,
+                 var image: Int,
+                 var header: Int,
+                 var sinopsis: String) {
+}
